7780571228497f333e82e8d0812ad5a3ae48c6cc
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-08-18 18:04:13 -0500

    added data directory

---
+++ aitesting/class.py
@@ -0,0 +1,13 @@
+"""_summary_
+adsads
+
+"""
+
+class Car:
+    """_summary_ asdasd
+    """
+    def __init__(self, make: str, model:str, year: int):
+        #Initialize the Car with specific attributes.
+        self.make = make
+        self.model = model
+        self.year = year
---
+++ tests/main.py
@@ -0,0 +1,10 @@
+"""Entry point for testing"""
+
+
+
+def main():
+    """_summary_
+    main
+    """
+    print('hello world')
+main()
